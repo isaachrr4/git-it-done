ee7d0eb3d3a7a3b8884c3374e904c9e00e1d4a9c
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-05-27 13:19:19 -0400

    link index and single html

--- assets/js/single.js
+++ assets/js/single.js
@@ -6,25 +6,40 @@ var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-  // make a get request to url
-  fetch(apiUrl).then(function(response) {
+ // make a get request to url
+fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
         displayIssues(data);
-
+  
         // check if api has paginated issues
         if (response.headers.get("Link")) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      console.log(response);
-      alert("There was a problem with your request!");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -81,4 +96,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
